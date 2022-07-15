dcefdf6c1167b1e78e469ad453dddec70042648d
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-15 09:23:08 +0500

    adding mapping

---
+++ TextEditor_NGSQL_DotNET/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TextEditor_NGSQL_DotNET.Entity;
+using TextEditor_NGSQL_DotNET.Model;
+
+namespace TextEditor_NGSQL_DotNET.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Content, Post>().ReverseMap();
+        }
+    }
+}

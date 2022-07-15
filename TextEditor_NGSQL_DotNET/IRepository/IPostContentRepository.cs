4d3ea4abf8b83ec10ea3cf075e0a4151f86afc89
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-15 10:10:47 +0500

    configuring repositories

---
+++ TextEditor_NGSQL_DotNET/IRepository/IPostContentRepository.cs
@@ -0,0 +1,8 @@
+﻿using TextEditor_NGSQL_DotNET.Entity;
+
+namespace TextEditor_NGSQL_DotNET.IRepository
+{
+    public interface IPostContentRepository: IGenericRepository<Post>
+    {
+    }
+}
---
+++ TextEditor_NGSQL_DotNET/Repository/PostContentRepository.cs
@@ -0,0 +1,13 @@
+﻿using TextEditor_NGSQL_DotNET.Context;
+using TextEditor_NGSQL_DotNET.Entity;
+using TextEditor_NGSQL_DotNET.IRepository;
+
+namespace TextEditor_NGSQL_DotNET.Repository
+{
+    public class PostContentRepository : GenericRepository<Post>, IPostContentRepository
+    {
+        public PostContentRepository(TxtEditorDbContext dbContext, Serilog.ILogger logger) : base(dbContext, logger)
+        {
+        }
+    }
+}
